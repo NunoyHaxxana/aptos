2c16355151c46dc3c48bd3d98589a1ae41db8ec5
Nunoy <83507970+NunoyHaxxana@users.noreply.github.com>
2022-05-21 01:19:33 +0700

    Update up_aptos_0_1_1.sh

--- up_aptos_0_1_1.sh
+++ up_aptos_0_1_1.sh
@@ -4,11 +4,12 @@ sudo rm -rf /usr/local/bin/aptos/
 sudo rm -rf /usr/bin/aptos
 
 wget -qO aptos-cli-0.1.1-Ubuntu-x86_64.zip https://github.com/aptos-labs/aptos-core/releases/download/aptos-cli-v0.1.1/aptos-cli-0.1.1-Ubuntu-x86_64.zip
-sudo unzip -o aptos-cli-0.1.1-Ubuntu-x86_64.zip
+sudo unzip -o aptos-cli-0.1.1-Ubuntu-x86_64.zip &
 
 sudo mv aptos /usr/local/bin/ 
 sudo chmod +x /usr/local/bin/aptos
 sudo ln -s /usr/local/bin/aptos /usr/bin/aptos
  
-
+echo -e " "
+echo -e " "
 echo -e "Update aptos-cli-0.1.1 \e[32mComplete \e[39m!"
